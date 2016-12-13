bc0058990744052f06d9ff2a2bde57b3b136308a
WillySung <wanny820514@gmail.com>
2016-12-13 08:31:53 +0000

    first commit 1.Take photos of chessboard from left and right camera at the same time. 2.You can choose auto snapshot or press "s" to snapshot.

---
+++ calibrate/shot.py
@@ -0,0 +1,52 @@
+import cv2
+import time
+
+AUTO = False  #set ture to auto snap shot
+INTERVAL = 2  #snap shot time interval = 2 seconds
+
+cv2.namedWindow("left")
+cv2.namedWindow("right")
+cv2.moveWindow("left",0,0)
+cv2.moveWindow("right",800,0)
+
+left_camera = cv2.VideoCapture(0)
+right_camera = cv2.VideoCapture(1)
+
+counter = 0
+utc = time.time()
+pattern = (8,6)   #the size of chessboard
+
+def snap_shot(pos,frame):
+    global counter
+    filename = pos + "_" + str(counter) + ".jpg"
+
+    cv2.imwrite(filename,frame)
+    print(filename + " saved:")
+
+while True:
+    ret, left_frame = left_camera.read()
+    ret, right_frame = right_camera.read()
+
+    cv2.imshow("left", left_frame)
+    cv2.imshow("right", right_frame)
+
+    #auto snap shot mode
+    now = time.time()
+    if AUTO and now-utc >= INTERVAL:
+         snap_shot("left", left_frame)
+         snap_shot("right", right_frame)
+         counter += 1
+         utc = now
+    
+    #press s to snap shot
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord("q"):
+         break
+    elif key & 0xFF == ord("s"):
+         snap_shot("left", left_frame)
+         snap_shot("right", right_frame)
+         counter += 1
+
+left_camera.release()
+right_camera.release()
+cv2.destroyAllWindows()
--- stereo_matching.py
+++ stereo_matching.py
@@ -1,13 +1,27 @@
+# -*- coding: utf-8 -*-
 import cv2
 import numpy as np
 
-def main():
+capL = cv2.VideoCapture(0)
+capR = cv2.VideoCapture(1)
+imgL = np.zeros((480,640,3),np.uint8)
+imgR = np.zeros((480,640,3),np.uint8)
+
+while(True):
+     capL.read(imgL)
+     capR.read(imgR)
+     gray_l = cv2.pyrDown(cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY))
+     gray_r = cv2.pyrDown(cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY))
+
+     '''#for single image
      gray_l = cv2.imread('aloeL.jpg',0)
      gray_r = cv2.imread('aloeR.jpg',0)
      gray_l = cv2.pyrDown(gray_l)  # downscale images for faster processing
      gray_r = cv2.pyrDown(gray_r)
-
-     window_size = 3
+     '''
+     '''
+     #StereoSGBM setting
+     window_size = 5
      min_disp = 16
      num_disp = 112-min_disp
      stereo = cv2.StereoSGBM_create(
@@ -20,16 +34,20 @@ def main():
         uniquenessRatio = 10,      
         speckleWindowSize = 100,   
         speckleRange = 32,         
-     )
+     )'''
 
-     #stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
-     disp = stereo.compute(gray_l,gray_r).astype(np.float32) / 16.0    
+     stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+     disp = stereo.compute(gray_l,gray_r)  
+
+     #disp = stereo.compute(gray_l,gray_r).astype(np.float32) / 16.0    
 
      cv2.imshow('gray_l',gray_l)
      cv2.imshow('gray_r',gray_r)
-     cv2.imshow('disp', (disp-min_disp)/num_disp)
-     cv2.waitKey(0)
-     cv2.destroyAllWindows()
+     cv2.imshow('disp', disp/255)
      
-if __name__ == '__main__':
-      main()   
+     if cv2.waitKey(1) & 0xFF == ord('q'): break 
+
+capL.release()
+capR.release()
+cv2.destroyAllWindows()
+     
